ab811ca21c5551e015964f9f302b12f4eb197c49
jalewis1 <jalewis1@pyrite-n2.cs.iastate.edu>
2021-11-08 09:43:49 -0600

    Added clubdetails and login create screens and code

---
+++ Frontend/app/src/main/java/com/example/isugroups/ClubDetailsScreen.java
@@ -0,0 +1,146 @@
+package com.example.isugroups;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ClubDetailsScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_details_screen);
+
+        ImageButton back = (ImageButton) findViewById(R.id.Back);//back button
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Back to the previous page
+                startActivity(new Intent(ClubDetailsScreen.this, CreateLoginScreen.class));
+            }
+        });
+
+
+        Button join = (Button) findViewById(R.id.JoinButton);
+
+        Boolean isMember = false;
+        //check if user is member in the DM
+        //TODO
+        if (isMember) {
+            join.setVisibility(View.GONE);
+        }
+
+
+        join.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                //Join Club, add user ID to the list and refresh page to change view
+                startActivity(new Intent(ClubDetailsScreen.this, ClubDetailsScreen.class));
+                //TODO
+            }
+        });
+
+
+        String ClubNameData = "Random Club";//Will be called from DB
+
+        TextView ClubName = (TextView) findViewById(R.id.ClubName);
+        ClubName.setText(ClubNameData);
+
+        String DescriptionData = "Club promoting and celebrating the game of chess.";//Will be called by DB
+
+        TextView Description = (TextView) findViewById(R.id.Description);
+        Description.setText(DescriptionData);
+
+
+        String MeetTimeData = "Meetings are on MWF 3:00 - 5:00 p.m. and R 6:00 - 7:00 p.m.";//Will be called by DB
+
+        TextView MeetTime = (TextView) findViewById(R.id.MeetingTimes);
+        MeetTime.setText(MeetTimeData);
+
+        String EventData = "Event information is delivered at club meetings, over email, and through this app.";
+        Button yourButton = (Button) findViewById(R.id.EventButton);
+
+        yourButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                //Go to events page
+                //TODO
+            }
+        });
+
+        TextView Event = (TextView) findViewById(R.id.Events);
+        Event.setText(EventData);
+
+        int numMembers = 0;
+        String Rescrictions = "";
+        String Elections = "";
+        String Positions = "";
+
+        String MemberData = "Students                             73\n" +
+                "ISU members                     84\n" +
+                "Non-ISU members            14\n" +
+                "\n" +
+                "Membership Restrictions\n" +
+                "None\n" +
+                "\n" +
+                "Officer Positions\n" +
+                "President\n" +
+                "Treasurer\n" +
+                "\n" +
+                "Elections\n" +
+                "Held in November\n" +
+                "\n";
+
+        TextView Member = (TextView) findViewById(R.id.Members);
+        Member.setText(MemberData);
+
+        Button yourButton2 = (Button) findViewById(R.id.EventButton);
+
+        yourButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                //Download Constitution from DB
+                //TODO
+            }
+        });
+
+        int FeeNum = 0;
+
+        String FeeData = "$90 Per Semester\n" +
+                "\n" +
+                "$180 Per Year";
+
+        TextView fee = (TextView) findViewById(R.id.Fees);
+        fee.setText(FeeData);
+
+        String OfficerList = "";
+
+        String OfficersData = "President\n" +
+                "John Doe\n" +
+                "Treasurer\n" +
+                "Jane Doe";
+
+        TextView Officers = (TextView) findViewById(R.id.Officers);
+        Officers.setText(OfficersData);
+
+        String ContactList = "";
+
+        String ContactData = "John Doe\n" +
+                "Phone Number: 123-456-7890\n" +
+                "email: example@gmail.com\n" +
+                "Jane Doe\n" +
+                "Phone Number: 123-456-7891\n" +
+                "email: example2@gmail.com";
+
+        TextView Contact = (TextView) findViewById(R.id.Contact);
+        Contact.setText(ContactData);
+
+
+    }
+
+}
---
+++ Frontend/app/src/main/java/com/example/isugroups/CreateLoginScreen.java
@@ -0,0 +1,80 @@
+package com.example.isugroups;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class CreateLoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_login_screen);
+
+        ImageButton yourButton = (ImageButton) findViewById(R.id.BackToLogin);
+
+        yourButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateLoginScreen.this, ClubDetailsScreen.class));
+            }
+        });
+
+
+        Button Button = (android.widget.Button) findViewById(R.id.CreateUserSumbit);
+
+        Button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                EditText firstName1 = (EditText)findViewById(R.id.FirstName);
+                String firstName = firstName1.getText().toString();
+
+                EditText lastName1 = (EditText)findViewById(R.id.LastName);
+                String lastName = lastName1.getText().toString();
+
+                EditText username1 = (EditText)findViewById(R.id.Username);
+                String username = username1.getText().toString();
+
+                EditText password1 = (EditText)findViewById(R.id.Password);
+                String password = password1.getText().toString();
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(CreateLoginScreen.this);
+                builder1.setMessage("Entered values are: \n" + firstName + "\n" + lastName
+                        + "\n" + username + "\n" + password );
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+
+            }
+        });
+
+
+    }
+
+}
