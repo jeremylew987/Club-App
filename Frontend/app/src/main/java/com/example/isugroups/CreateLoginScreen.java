10df461a7ee2cf30354271a7053d918a20b1cd17
jalewis1 <jalewis1@pyrite-n1.cs.iastate.edu>
2021-11-10 19:42:52 -0600

    global varibles to be stored and added some json code to request from backend

--- Frontend/app/src/main/java/com/example/isugroups/CreateLoginScreen.java
+++ Frontend/app/src/main/java/com/example/isugroups/CreateLoginScreen.java
@@ -12,6 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class CreateLoginScreen extends AppCompatActivity {
 
     @Override
@@ -48,28 +54,37 @@ public class CreateLoginScreen extends AppCompatActivity {
                 EditText password1 = (EditText)findViewById(R.id.Password);
                 String password = password1.getText().toString();
 
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(CreateLoginScreen.this);
-                builder1.setMessage("Entered values are: \n" + firstName + "\n" + lastName
-                        + "\n" + username + "\n" + password );
-                builder1.setCancelable(true);
+                RequestQueue queue = Volley.newRequestQueue(CreateLoginScreen.this);
+                JSONObject data = new JSONObject();
+                try {
+                    data.put("username",username);
+                    data.put("passphrase",password);
+                    data.put("firstName",firstName);//firstname
+                    data.put("lastName",lastName);//last name
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+				/*JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, Constance.BASE_URL + "user/login", data, new Response.Listener<JSONObject>() {
+					@Override
+					public void onResponse(JSONObject response) {
+						Intent i = new Intent(LoginScreen.this,MainMenuScreen.class);
+						startActivity(i);
+					}
+				}, new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						error.printStackTrace();
+						AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(LoginScreen.this);
+						alertDialogBuilder.setTitle("Error");
+						alertDialogBuilder.setMessage(error.getMessage());
+						alertDialogBuilder.setPositiveButton("Ok", null);
+						alertDialogBuilder.setNegativeButton("", null);
+						alertDialogBuilder.create().show();
+					}
+				});
+				queue.add(request);*/
 
-                builder1.setPositiveButton(
-                        "Yes",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
 
-                builder1.setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
 
             }
         });
