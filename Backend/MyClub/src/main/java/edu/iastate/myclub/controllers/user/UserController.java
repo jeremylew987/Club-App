4e07c7060ed7ecd88cc44bc3604b506fda7e459b
Benjamin Riermersma <btr@iastate.edu>
2021-11-16 21:38:42 -0600

    added Users to backend

--- Backend/MyClub/src/main/java/edu/iastate/myclub/controllers/user/UserController.java
+++ Backend/MyClub/src/main/java/edu/iastate/myclub/controllers/user/UserController.java
@@ -1,5 +1,38 @@
 package edu.iastate.myclub.controllers.user;
 
-public class UserController {
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import edu.iastate.myclub.models.*;
+import edu.iastate.myclub.services.*;
+import edu.iastate.myclub.services.user.UserService;
+import edu.iastate.myclub.models.user.User;
+
+@RestController
+@RequestMapping(path="/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/create")
+	public ResponseEntity<Boolean> createUser(@RequestHeader HttpHeaders headers, @RequestBody User user){
+		return new ResponseEntity<Boolean>(userService.createUser(user),HttpStatus.OK);
+	}
+	
+	@GetMapping("/get")
+	public ResponseEntity<User> getUser(@RequestHeader HttpHeaders headers, @RequestParam("name") String name ){
+		return new ResponseEntity<User>(userService.getUser(name), HttpStatus.OK);
+	}
 }
--- Backend/MyClub/src/main/java/edu/iastate/myclub/repos/user/UserRepository.java
+++ Backend/MyClub/src/main/java/edu/iastate/myclub/repos/user/UserRepository.java
@@ -1,5 +1,14 @@
 package edu.iastate.myclub.repos.user;
 
-public class UserRepository {
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.iastate.myclub.models.user.User;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User,Integer>{
+	List<User> findAllUsersByName(String name, Pageable pageable);
 }
